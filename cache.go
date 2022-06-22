f5c1977b75ad6ee81c6c311ef83e77860fc6a3ef
viktor <viktor@localhost>
2022-06-22 20:25:01 +0400

    solution

--- cache.go
+++ cache.go
@@ -1,23 +1,94 @@
 package cache
 
-import "time"
+import (
+	"time"
+)
+
+type Value struct {
+	key, value string
+	deadline   *time.Time
+}
+
+func NewValue(key, value string, deadline *time.Time) Value {
+	return Value{key: key, value: value, deadline: deadline}
+}
+
+func (v Value) IsExpired() bool {
+	if v.deadline != nil && v.deadline.Before(time.Now()) {
+		return true
+	}
+
+	return false
+}
 
 type Cache struct {
+	values []Value
 }
 
 func NewCache() Cache {
 	return Cache{}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (c *Cache) remove(key string) {
+	var values []Value
 
+	for _, v := range c.values {
+		if v.key != key {
+			values = append(values, v)
+		}
+	}
+
+	c.values = values
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Get(key string) (string, bool) {
+	c.Cleanup()
+
+	for _, v := range c.values {
+		if v.key == key {
+			return v.value, true
+		}
+	}
+
+	return "", false
 }
 
-func (receiver) Keys() []string {
+func (c *Cache) Set(key, value string, deadline *time.Time) {
+	for _, v := range c.values {
+		if v.key == key {
+			v.value = value
+			v.deadline = deadline
+			return
+		}
+	}
+
+	c.values = append(c.values, NewValue(key, value, deadline))
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) Put(key, value string) {
+	c.Set(key, value, nil)
+}
+
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	c.Set(key, value, &deadline)
+}
+
+func (c *Cache) Keys() []string {
+	c.Cleanup()
+
+	var keys []string
+
+	for _, v := range c.values {
+		keys = append(keys, v.key)
+	}
+
+	return keys
+}
+
+func (c *Cache) Cleanup() {
+	for _, v := range c.values {
+		if v.IsExpired() {
+			c.remove(v.key)
+		}
+	}
 }
